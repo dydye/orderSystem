eeb7e84c54e71b91044afca487cc868a5744fceb
daiyun <daiyun@tuzhanai.com>
2020-08-18 11:58:01 +0800

    initial user module and modify the configuration

--- user/src/main/java/com/ratel/user/UserApplication.java
+++ user/src/main/java/com/ratel/user/UserApplication.java
@@ -1,4 +1,4 @@
-package com.ratel.product;
+package com.ratel.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
---
+++ user/src/test/java/com/ratel/user/UserApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.ratel.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
